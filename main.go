717060b44a028c6920195dc7442553174de0a927
Hannah.Alemu <hnnh.almu@gmail.com>
2022-01-24 17:55:33 +0000

    all

---
+++ main.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("letters.txt")
+	if err != nil {
+		log.Fatalf("ERROR: %s", err)
+	}
+
+	// file to slice of string by line
+
+	var lttrlines []string
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		lttrlines = append(lttrlines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+
+	// find a string in a scanned file
+
+	// // Splits on newlines by default.
+	ln1 := ""
+	ln2 := ""
+	ln3 := ""
+	ln4 := ""
+	ln5 := ""
+	ln6 := ""
+	ln7 := ""
+	ln8 := ""
+
+	line := 0
+	// dash := 0
+	Output := os.Args[1]
+	SlcOutput := []rune(Output)
+	// count := 0
+	// fmt.Println(Output)
+	// if strings.Contains(Output, "\\")
+	dash := strings.Index(Output, `\n`)
+	// fmt.Println(dash)
+	for i := 0; i < len(SlcOutput); i++ {
+		// fmt.Println(count)
+		// count++
+		// fmt.Print(string(c))
+		// fmt.Println(i)
+
+		// 	break
+		// }
+		// dash++
+
+		if i == dash && dash >= 0 {
+			if ln1 != "" && ln2 != "" && ln3 != "" && ln4 != "" && ln5 != "" && ln6 != "" && ln7 != "" && ln8 != "" {
+				fmt.Println(ln1)
+				fmt.Println(ln2)
+				fmt.Println(ln3)
+				fmt.Println(ln4)
+				fmt.Println(ln5)
+				fmt.Println(ln6)
+				fmt.Println(ln7)
+				fmt.Println(ln8)
+			} else {
+				fmt.Println()
+			}
+
+			ln1 = ""
+			ln2 = ""
+			ln3 = ""
+			ln4 = ""
+			ln5 = ""
+			ln6 = ""
+			ln7 = ""
+			ln8 = ""
+			dash = dash + strings.Index(Output[dash+1:], `\n`) + 1
+			// fmt.Println(dash)
+			i++
+			// fmt.Println(i)
+
+		} else {
+			for j, s := range lttrlines {
+
+				line = j
+				runes := []rune(s)
+				if len(s) > 0 && runes[0] == SlcOutput[i] {
+					break
+				}
+
+			}
+
+			for i := line + 1; i <= line+8; i++ {
+				if i == line+1 {
+					ln1 = ln1 + lttrlines[i]
+				}
+				if i == line+2 {
+					ln2 = ln2 + lttrlines[i]
+				}
+				if i == line+3 {
+					ln3 = ln3 + lttrlines[i]
+				}
+				if i == line+4 {
+					ln4 = ln4 + lttrlines[i]
+				}
+				if i == line+5 {
+					ln5 = ln5 + lttrlines[i]
+				}
+				if i == line+6 {
+					ln6 = ln6 + lttrlines[i]
+				}
+				if i == line+7 {
+					ln7 = ln7 + lttrlines[i]
+				}
+				if i == line+8 {
+					ln8 = ln8 + lttrlines[i]
+				}
+
+			}
+			if i == len(Output)-1 {
+				fmt.Println(ln1)
+				fmt.Println(ln2)
+				fmt.Println(ln3)
+				fmt.Println(ln4)
+				fmt.Println(ln5)
+				fmt.Println(ln6)
+				fmt.Println(ln7)
+				fmt.Println(ln8)
+				ln1 = ""
+				ln2 = ""
+				ln3 = ""
+				ln4 = ""
+				ln5 = ""
+				ln6 = ""
+				ln7 = ""
+				ln8 = ""
+			}
+		}
+
+		// if c == ' ' {
+		// 	ln1 = ln1 + "      "
+		// 	ln2 = ln2 + "      "
+		// 	ln3 = ln3 + "      "
+		// 	ln4 = ln4 + "      "
+		// 	ln5 = ln5 + "      "
+		// 	ln6 = ln6 + "      "
+		// 	ln7 = ln7 + "      "
+		// 	ln8 = ln8 + "      "
+		// }
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+		// 	// Handle the error
+	}
+
+	file.Close()
+}
