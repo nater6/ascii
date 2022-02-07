7e24993f3d9d32fec984fd9f40aecbb85b5df066
Hannah.Alemu <hnnh.almu@gmail.com>
2022-02-07 12:30:45 +0000

    reverse

--- map/main.go
+++ map/main.go
@@ -1,7 +1,42 @@
-package kap
+package main
 
-import "ascii"
+import (
+	"ascii"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
-func Somethjung() {
-	ascii.AsciiMap()
+func main() {
+	file, err := os.Open("standard.txt")
+	if err != nil {
+		log.Fatalf("ERROR: %s", err)
+	}
+
+	// file to slice of string by line
+
+	var lttrlines []string
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		lttrlines = append(lttrlines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+	// loop through file
+	mapslice := ascii.Createmap(lttrlines)
+	Output := os.Args[1]
+
+	ascii.Condition(mapslice, Output)
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+		// Handle the error
+	}
+
+	file.Close()
 }
--- reverse/reverse.go
+++ reverse/reverse.go
@@ -9,13 +9,13 @@ import (
 )
 
 func main() {
+	// ASCII ART file open
 	file, err := os.Open("letters.txt")
 	if err != nil {
 		log.Fatalf("ERROR: %s", err)
 	}
 
-	// file to slice of string by line
-
+	// ASCII ART file to slice of string by line
 	var lttrlines []string
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
@@ -28,56 +28,52 @@ func main() {
 		fmt.Println(err)
 	}
 
-	// new file to read
-	fmt.Println(os.Args[1][10:])
+	// INPUT FILE open
+	// // fmt.Println(os.Args[1][10:])
 	reverse, err := os.Open(os.Args[1][10:])
 	if err != nil {
 		fmt.Println("Usage: go run . [OPTION]")
 		fmt.Print("EX: go run . --reverse=<fileName>")
 		return
 	}
-	// file to slice of string by line
 
+	// INPUT file to slice of string by line
 	var strreverse []string
 	scanner2 := bufio.NewScanner(reverse)
 	scanner2.Split(bufio.ScanLines)
-
 	for scanner2.Scan() {
 		strreverse = append(strreverse, scanner2.Text())
 	}
-
 	if err := scanner2.Err(); err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(strreverse)
-	// loop through file
-	mapslice := ascii.Createmap(lttrlines)
 
+	// create map of ascii art
+	mapslice := ascii.Createmap(lttrlines)
+	runewidth := []rune(strreverse[0])
 	checkascii := make([]string, 8)
 	output := ""
-	fmt.Println(len(strreverse))
 
-	for i := 0; i <= len(strreverse[0])-1; i++ {
-
+	// loop through each index of the first slice (first line) of the slices of the inpput file
+	for i := 0; i < len(runewidth); i++ {
+		// loop through each slice, which is each new line of the file
 		for j, line := range strreverse {
+			// add in the art by vertical line
 			slice := []rune(line)
 			checkascii[j] += string(slice[i])
-
 		}
-		match := true
 		for k, art := range mapslice {
 			count := 0
+			match := true
 			for num, slice := range art {
-				if num > 0 && slice == checkascii[count] {
+				if num != 0 && slice == checkascii[count] {
 					count++
-					fmt.Println("yay")
-				} else {
+				} else if num != 0 {
 					match = false
 					break
 				}
 			}
 			if match {
-				fmt.Println("yay2")
 				output += string(rune(k))
 				checkascii = make([]string, 8)
 				break
@@ -85,11 +81,6 @@ func main() {
 		}
 
 	}
-
-	// Output := os.Args[1]
-
-	// art := ascii.Condition(mapslice, Output)
-	// ascii.Condition(mapslice, Output)
 	fmt.Println(output)
 
 	if err := scanner.Err(); err != nil {
